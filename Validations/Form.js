ab63785dc9bcbd179f83c4090281fdbe8c921137
Anton1oo <lipesouza7392@gmail.com>
2023-10-05 20:41:22 -0300

    Criado a validação da tela de cadastro

--- Validations/Form.js
+++ Validations/Form.js
@@ -4,3 +4,9 @@ export const loginValidationSchema = yup.object().shape({
     Email: yup.string().email("Por favor insira um email valido"),
     Password: yup.string().min(8,({min})=> `A senha deve ter no mínimo ${min} digitos`)
 })
+
+export const cadastroValidationSchema= yup.object().shape({
+    Email: yup.string().email("Por favor insira um email valido"),
+    Password: yup.string().min(8,({min})=> `A senha deve ter no mínimo ${min} digitos`),
+    CPF: yup.string().min(11, ({min})=>`Coloque um CPF valido com no minino ${min} digitos`)
+})
